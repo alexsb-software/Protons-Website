e02423976dc2ce44bc7ea7b353792a3fb240500a
TarekAlQaddy <tarek.alqaddy@gmail.com>
2017-03-30 05:42:32 +0200

    home responsive, graduates structure

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@
  * Created by Tarek AlQaddy on 3/29/2017.
  */
 $(document).ready(function () {
+    $('#button-collapse').sideNav();
     $('#slick').slick({
         dots:true,
         autoplay:true
