87320bd9d6c689d867dd602bb0118f9ea6fce65a
TarekAlQaddy <tarek.alqaddy@gmail.com>
2017-04-13 01:44:12 +0200

    additional edits :fire:
    add tracks remaining icons
    add footer to every page,
    modify graduates structure,
    add FAQs prototype page

--- js/home.js
+++ js/home.js
@@ -2,7 +2,6 @@
  * Created by Tarek AlQaddy on 3/29/2017.
  */
 $(document).ready(function () {
-    $('#button-collapse').sideNav();
     $('#slick').slick({
         dots:true,
         autoplay:true
