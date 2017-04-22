54df65fb46d442f878821f6a3f948661af2dbca2
tarekalqaddy <tarek.alqaddy@gmail.com>
2017-04-23 00:26:08 +0200

    FAQs page, dynamically load data from js

--- js/faqs.js
+++ js/faqs.js
@@ -11,4 +11,47 @@ var faqs = {
         { question: 'What is Protons ?', answer: 'Protons is a program organized by IEEE AlexSB, it targets school students in preparatory and secondary stages with different backgrounds. It mainly aims to raise the participants’ knowledge, let them learn in a motivational atmosphere and help them discover their abilities and inner motive.' },
         { question: 'What will I gain from joining protons other than the technical knowledge ?', answer: 'The program focuses on the non-technical skills as much as the technical ones, through workshops that develop the participants’ communication, teamwork, time management, leadership and presentation skills.' }
     ]
-}
+};
+
+$(document).ready(function () {
+    var cat =["Place", "Time", "Others"];
+
+    var faqsSec = document.getElementById("faqs-sec");
+
+    for(var i = 0; i<cat.length ; i++){
+        var faqsBlock  = document.createElement('div');
+
+        var head = document.createElement('h2');
+        head.setAttribute('class', 'faqs-head');
+        head.innerHTML = cat[i];
+
+        faqsBlock.appendChild(head);
+
+        var ul = document.createElement('ul');
+        ul.setAttribute('class', 'collapsible center-block');
+        ul.setAttribute('data-collapsible', 'accordion');
+
+        for(var j = 0; j<faqs[cat[i]].length; j++){
+            var li = document.createElement('li');
+
+            var question = document.createElement('div');
+            question.setAttribute('class', 'collapsible-header');
+            question.innerHTML = faqs[cat[i]][j].question;
+
+            var answer = document.createElement('div');
+            answer.setAttribute('class', 'collapsible-body');
+            answer.innerHTML = faqs[cat[i]][j].answer;
+
+            li.appendChild(question);
+            li.appendChild(answer);
+
+            ul.appendChild(li)
+        }
+
+        faqsBlock.appendChild(ul);
+        faqsSec.appendChild(faqsBlock);
+    }
+
+
+});
+
--- js/graduates.js
+++ js/graduates.js
@@ -1,51 +1,103 @@
-var graduates = {
-    "2016": [
-        { name: 'Abdelrahman Refaat', image: '' },
-        { name: 'Abdelrahman Mady', image: '' },
-        { name: 'Adham Shady', image: '' },
-        { name: 'Ahmed Hossam', image: '' },
-        { name: 'Ahmed Kamar', image: '' },
-        { name: 'Ahmed Ossama', image: '' },
-        { name: 'Ahmed Salah', image: '' },
-        { name: 'Aya Mohi', image: '' },
-        { name: 'Basma Mohamed Kassem', image: '' },
-        { name: 'Ezzelden Mohamed Elmasre', image: '' },
-        { name: 'Fatma Hamdy', image: '' },
-        { name: 'Hana Ghanem', image: '' },
-        { name: 'Kirollos Adel', image: '' },
-        { name: 'Lina Samy', image: '' },
-        { name: 'Mahmoud Alshemy', image: '' },
-        { name: 'Mahmoud Gamal', image: '' },
-        { name: 'Malak Farhoud', image: '' },
-        { name: 'Mark Adel', image: '' },
-        { name: 'Marwan Fouda', image: '' },
-        { name: 'Mazen AboelHassan', image: '' },
-        { name: 'Menna Nabil', image: '' },
-        { name: 'Mina Ramiz', image: '' },
-        { name: 'Mohamed AbdelMoniem', image: '' },
-        { name: 'Mohamed AbdelSamie', image: '' },
-        { name: 'Mohamed Ashraf', image: '' },
-        { name: 'Mostafa Magdy', image: '' },
-        { name: 'Moustafa Omar', image: '' },
-        { name: 'Nermiin Evie', image: '' },
-        { name: 'Noha Haydak', image: '' },
-        { name: 'Nourhan Farouk', image: '' },
-        { name: 'Omar Hisham', image: '' },
-        { name: 'Pola Ehab', image: '' },
-        { name: 'Ramez Aziz', image: '' },
-        { name: 'Rewan Rony', image: '' },
-        { name: 'Salah Kamal', image: '' },
-        { name: 'Salma Amr', image: '' },
-        { name: 'Salma Hisham', image: '' },
-        { name: 'Sara Tarek Mohamed', image: '' },
-        { name: 'Sayfnaz Tarek', image: '' },
-        { name: 'Shahd Mahmoud Mohamed', image: '' },
-        { name: 'Toka Sherif', image: '' },
-        { name: 'Waled Mostafa Aly', image: '' },
-        { name: 'Yosra Emad', image: '' },
-        { name: 'Youssef Reda', image: '' }
-    ],
-    "2015": [
+var graduates = [
+    {
+        year: 2016,
+        data: [
+            { name: 'Abdelrahman Refaat', image: '' },
+            { name: 'Abdelrahman Mady', image: '' },
+            { name: 'Adham Shady', image: '' },
+            { name: 'Ahmed Hossam', image: '' },
+            { name: 'Ahmed Kamar', image: '' },
+            { name: 'Ahmed Ossama', image: '' },
+            { name: 'Ahmed Salah', image: '' },
+            { name: 'Aya Mohi', image: '' },
+            { name: 'Basma Mohamed Kassem', image: '' },
+            { name: 'Ezzelden Mohamed Elmasre', image: '' },
+            { name: 'Fatma Hamdy', image: '' },
+            { name: 'Hana Ghanem', image: '' },
+            { name: 'Kirollos Adel', image: '' },
+            { name: 'Lina Samy', image: '' },
+            { name: 'Mahmoud Alshemy', image: '' },
+            { name: 'Mahmoud Gamal', image: '' },
+            { name: 'Malak Farhoud', image: '' },
+            { name: 'Mark Adel', image: '' },
+            { name: 'Marwan Fouda', image: '' },
+            { name: 'Mazen AboelHassan', image: '' },
+            { name: 'Menna Nabil', image: '' },
+            { name: 'Mina Ramiz', image: '' },
+            { name: 'Mohamed AbdelMoniem', image: '' },
+            { name: 'Mohamed AbdelSamie', image: '' },
+            { name: 'Mohamed Ashraf', image: '' },
+            { name: 'Mostafa Magdy', image: '' },
+            { name: 'Moustafa Omar', image: '' },
+            { name: 'Nermiin Evie', image: '' },
+            { name: 'Noha Haydak', image: '' },
+            { name: 'Nourhan Farouk', image: '' },
+            { name: 'Omar Hisham', image: '' },
+            { name: 'Pola Ehab', image: '' },
+            { name: 'Ramez Aziz', image: '' },
+            { name: 'Rewan Rony', image: '' },
+            { name: 'Salah Kamal', image: '' },
+            { name: 'Salma Amr', image: '' },
+            { name: 'Salma Hisham', image: '' },
+            { name: 'Sara Tarek Mohamed', image: '' },
+            { name: 'Sayfnaz Tarek', image: '' },
+            { name: 'Shahd Mahmoud Mohamed', image: '' },
+            { name: 'Toka Sherif', image: '' },
+            { name: 'Waled Mostafa Aly', image: '' },
+            { name: 'Yosra Emad', image: '' },
+            { name: 'Youssef Reda', image: '' }
+        ]
+    },
+    {
+        year: 2015,
+        data:[
 
-    ]
-}
+        ]
+    }
+];
+
+$(document).ready(function () {
+    $('#button-collapse').sideNav();
+
+    var grads = $('#grads-cont');
+
+    for(var year =0; year< graduates.length; year++){
+
+        var graduatesSection = document.createElement('section');
+        graduatesSection.setAttribute('class', 'year-graduates');
+
+        graduatesSection.innerHTML = '<div class="row"><h2 class="col l4 offset-l1 m8 offset-m2 s12 year">' + graduates[year].year+ '</h2></div>';
+
+        var row = document.createElement('div');
+        row.setAttribute('class','row grad-row');
+
+        for(var i=0; i < graduates[year].data.length; i++){
+
+            if(i%6 === 0 && i !== 0){
+                graduatesSection.appendChild(row);
+                row = document.createElement('div');
+                row.setAttribute('class','row grad-row');
+            }
+
+            var proton = document.createElement('div');
+            proton.setAttribute('class', 'col l2 m4 s6 center-align center-block');
+
+            var protonImg = document.createElement('img');
+            protonImg.src = graduates[year].data[i].image === ''? 'media/graduates/user.png' : graduates[year].data[i].image;
+
+            var protonName = document.createElement('h5');
+            protonName.setAttribute('class', 'flow-text');
+            protonName.innerHTML = graduates[year].data[i].name;
+
+            proton.appendChild(protonImg);
+            proton.appendChild(protonName);
+
+            row.appendChild(proton)
+
+        }
+        graduatesSection.appendChild(row);
+
+        grads.append(graduatesSection)
+
+    }
+});
